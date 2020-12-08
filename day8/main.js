4ee1b6bfab6065a9ae62fd49ea03a54a187dd815
Markus Rutström <markusrutstrom@gmail.com>
2020-12-08 08:03:06 +0100

    Day 8 Part 2

--- day8/main.js
+++ day8/main.js
@@ -5,36 +5,84 @@ function calc(input) {
   let resultOne = 0;
   let resultTwo = 0;
 
+  ({ accumulator } = testInputForSuccess(input));
+  resultOne = accumulator;
+
+  var foundSuccess = false;
+  var lastChangedIndex = -1;
+  while (!foundSuccess) {
+    ({ inputWithChange, changedIndex } = getInputWithNewChange(input, lastChangedIndex));
+    lastChangedIndex = changedIndex;
+    
+    ({ success, accumulator } = testInputForSuccess(inputWithChange));
+    foundSuccess = success;
+  }
+  resultTwo = accumulator;
+
+  resultOneElement.innerHTML = resultOne;
+  resultTwoElement.innerHTML = resultTwo;
+}
+
+function getInputWithNewChange(input, latestChangedIndex) {
+  var indexToChange = findNextIndexToChange(input, latestChangedIndex);
+  var inputCopy = getArrayCopy(input);
+  var inputRowToChange = inputCopy[indexToChange];
+  changeInput(inputRowToChange);
+
+  return { inputWithChange: inputCopy, changedIndex: indexToChange };
+}
+
+function getArrayCopy(input) {
+  return JSON.parse(JSON.stringify(input));
+}
+
+function findNextIndexToChange(input, latestChangedIndex) {
+  return input.findIndex(
+    (inputRow, index) =>
+      (inputRow.action == "nop" || inputRow.action == "jmp") &&
+      index > latestChangedIndex
+  );
+}
+
+function changeInput(inputRowToChange) {
+  if (inputRowToChange.action === "nop") inputRowToChange.action = "jmp";
+  else if (inputRowToChange.action === "jmp") inputRowToChange.action = "nop";
+}
+
+function testInputForSuccess(input) {
   var visitedIndexes = [];
   var accumulator = 0;
   var index = 0;
+
   while (index >= 0 && index < input.length) {
     if (visitedIndexes.includes(index)) break;
 
     visitedIndexes.push(index);
     const actionRow = input[index];
-    doAction(actionRow);
+    ({ index, accumulator } = doAction(actionRow, index, accumulator));
   }
 
-  resultOneElement.innerHTML = accumulator;
-  resultTwoElement.innerHTML = resultTwo;
+  const success = index === input.length;
+  return { success, accumulator };
+}
 
-  function doAction(actionRow) {
-    switch (actionRow.action) {
-      case "nop":
-        index++;
-        break;
-      case "acc":
-        accumulator += actionRow.value;
-        index++;
-        break;
-      case "jmp":
-        index = index + actionRow.value;
-        break;
-      default:
-        break;
-    }
+function doAction(actionRow, index, accumulator) {
+  switch (actionRow.action) {
+    case "nop":
+      index++;
+      break;
+    case "acc":
+      accumulator += actionRow.value;
+      index++;
+      break;
+    case "jmp":
+      index = index + actionRow.value;
+      break;
+    default:
+      break;
   }
+
+  return { index, accumulator };
 }
 
 function log(string) {
